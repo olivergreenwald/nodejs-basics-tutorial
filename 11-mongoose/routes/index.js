633ce51510b5272fead1eb517b38b8e1c81ba82c
olivergreenwald <olivergreenwald@Olivers-MacBook-Pro-2.local>
2017-11-30 15:00:45 -0700

    finished last video on mongoose

--- 11-mongoose/routes/index.js
+++ 11-mongoose/routes/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+//connect the path to the database
 mongoose.connect('localhost:27017/test');
+//Schemas tell Mongoose what the data looks like and how to use it
 var Schema = mongoose.Schema;
 
+  //this defines the schema or makes a blueprint for it
 var userDataSchema = new Schema({
+  //you can make objects required like below
   title: {type: String, required: true},
   content: String,
   author: String
+  //you can force a collection came like below
 }, {collection: 'user-data'});
 
+//entering actual model 
 var UserData = mongoose.model('UserData', userDataSchema);
 
 /* GET home page. */
@@ -19,6 +25,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/get-data', function(req, res, next) {
   UserData.find()
+  //.then handles the data
       .then(function(doc) {
         res.render('index', {items: doc});
       });
@@ -32,6 +39,7 @@ router.post('/insert', function(req, res, next) {
   };
 
   var data = new UserData(item);
+  //saves the data to the database
   data.save();
 
   res.redirect('/');
@@ -41,6 +49,7 @@ router.post('/update', function(req, res, next) {
   var id = req.body.id;
 
   UserData.findById(id, function(err, doc) {
+    //handles errors
     if (err) {
       console.error('error, no entry found');
     }
@@ -54,6 +63,7 @@ router.post('/update', function(req, res, next) {
 
 router.post('/delete', function(req, res, next) {
   var id = req.body.id;
+  //passes id and executes the method
   UserData.findByIdAndRemove(id).exec();
   res.redirect('/');
 });
